29271483f8e7ad220604ac06cd224ac203bf446f
shawneetz <shawnalfred0505@gmail.com>
2026-06-24 03:21:24 +0800

    task_service + task_router logic

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,1 +1,47 @@
 # Tasks router
+
+from os import statvfs_result
+import uuid
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.session import get_db
+from app.dependencies import get_current_user
+from app.models.user import User
+from app.models.task import TaskStatus, TaskPriority
+from app.schemas import task
+from app.schemas.task import TaskCreate, TaskUpdate, TaskRead, TaskReorder
+from app.services import task_service
+
+router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+@router.get("", response_model=list[TaskRead])
+async def list_tasks(
+    status: TaskStatus | None = Query(),
+    priority: TaskPriority | None = Query(),
+    tag_id: uuid.UUID | None = Query(),
+    search: str | None = Query(),
+    db: AsyncSession = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    return await task_service.get_all_task(db, current_user.id, status, priority, tag_id, search)
+
+@router.post("", response_model=TaskRead, status_code=201)
+async def create_task(data: TaskCreate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await task_service.create_task(db, current_user.id, data)
+
+# IMPORTANT: /reorder must be above /{task_id}
+@router.patch("/reorder", response_model=TaskRead)
+async def reorder_task(data: TaskReorder, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await task_service.reorder_task(db, data, current_user.id)
+
+@router.get("/{task_id}", response_model=TaskRead)
+async def get_task(task_id: uuid.UUID, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await task_service.get_task_by_id(db, task_id, current_user.id)
+
+@router.patch("/{task_id}", response_model=TaskRead)
+async def update_task(task_id: uuid.UUID, data: TaskUpdate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await task_service.update_task(db, task_id, current_user.id, data)
+
+@router.delete("/{task_id}", status_code=204)
+async def delete_task(task_id: uuid.UUID, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    await task_service.delete_task(db, task_id, current_user.id)
--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,1 +1,122 @@
 # Business logic for task operations
+from re import search
+from turtle import position, title
+from unittest import result
+import uuid
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.orm import selectinload
+from app.models.task import Task, TaskPriority, TaskStatus
+from app.models.tag import Tag, task_tags
+from app.schemas.task import TaskCreate, TaskRead, TaskReorder
+from app.core.exceptions import NotFoundException
+
+# Reassigning of positions 0, 1, 2... to all of the tasks in a column
+async def _renormalize_column(db: AsyncSession, user_id: uuid.UUID, status: TaskStatus, exclude_id: uuid.UUID | None = None):
+    task = select(Task).where(Task.user_id==user_id, Task.status==status).order_by(Task.position)
+    if exclude_id: 
+        task = task.where(Task.id != exclude_id)
+    
+    result = await db.execute(task)
+    tasks = list(result.scalars().all())
+    for i, t in enumerate(tasks): # reassign the id per task
+        t.position = i
+    
+async def reorder_task(db: AsyncSession, data: TaskReorder, user_id: uuid.UUID) -> Task:
+    task = await get_task_by_id(db, data.task_id, user_id)
+    old_status = task.status
+
+    task.status = data.new_status
+    task.position = data.new_position
+    await db.flush()
+
+    if old_status != data.new_status:
+        await _renormalize_column(db, user_id, old_status)
+
+    dest_result = await db.execute(select(Task).where(Task.user_id==user_id, Task.status==data.new_status, Task.id==task.id).order_by(Task.position))
+    dest_tasks = list(dest_result.scalars().all())
+    dest_tasks.insert(Task.position, task)
+
+    for i, t in enumerate(dest_tasks): # reassign the id per task
+        t.position = i
+
+    await db.commit()
+    await db.refresh(task)
+    return task
+
+# Get all task
+async def get_all_task(
+    db: AsyncSession, 
+    user_id: uuid.UUID, 
+    status: TaskStatus | None = None,
+    priority: TaskPriority | None = None, 
+    tag_id: uuid.UUID | None = None,
+    search: str | None = None,
+) -> list[Task]:
+
+    task = select(Task).where(Task.user_id==user_id).options(selectinload(Task.tags))
+    if status:
+        task = task.where(Task.status==status)
+    if priority:
+        task = task.where(Task.priority==priority)
+    if tag_id:
+        task = task.where(Task.tag_id==tag_id)
+    if search:
+        task = task.where(Task.title.ilike(f"%{search}"))
+    task = task.order_by(Task.position)
+    result = await db.execute((task))
+
+    return list(result.scalars().all())
+    
+# Get task per id
+async def get_task_by_id(db: AsyncSession, task_id: uuid.UUID, user_id: uuid.UUID) -> Task:
+    task_selection = select(Task).where(Task.id==task_id, Task.user_id==user_id).options(selectinload(Task.tags))
+    result = await db.execute(task_selection)
+    task = result.scalar_one_or_none()
+    if not task:
+        raise NotFoundException("Task")
+    return task
+
+# Create task
+async def create_task(db: AsyncSession, user_id: uuid.UUID, data: TaskCreate) -> Task:
+    
+    task = Task(
+        user_id = user_id,
+        title = data.title,
+        descrition = data.description,
+        status = data.status,
+        priority = data.priority,
+        due_date = data.due_date,
+        position=0,
+    )
+
+    if data.tag_ids:
+        tag_result = await db.execute(select(Tag).where(Tag.id.in_(data.tag_ids), Tag.user_id == user_id))
+        task_tags = list(tag_result.scalars().all())
+
+    db.add(task)
+    await db.flush()
+    await _renormalize_column(db,user_id,data.status) 
+    await db.commit()
+    await db.refresh(task)
+    return task
+
+# Update task fields
+async def update_task(db: AsyncSession, task_id: uuid.UUID, user_id: uuid.UUID, data) -> Task:
+    task = await get_task_by_id(db, task_id, user_id)
+    for field, value in data.model_dump(exclude_unset=True).items():
+        if field == "tags_ids":
+            tags_result = await db.execute(select(Tag).where(Tag.id.in_(value), Tag.user_id==user_id))
+            task.tags = list(tags_result.scalars().all())
+        else:
+            setattr(task, field, value)
+    
+    await db.commit()
+    await db.refresh(task)
+    return task 
+
+# Delete task
+async def delete_task(db: AsyncSession, task_id: uuid.UUID, user_id: uuid.UUID) -> None:
+    task = await get_task_by_id(db, task_id, user_id)
+    await db.delete(task)
+    await db.commit()
